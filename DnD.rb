37839e70830c10af5d873472b3c77f959dc6e9be
davemerritt <davidmerritt6@gmail.com>
2013-11-03 17:56:01 -0500

    html file

--- DnD.rb
+++ DnD.rb
@@ -2,10 +2,11 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-File.open('DnD.txt', 'w') do |x|
+File.open('DnD.html', 'w') do |x|
 page = Nokogiri::HTML(open("http://www.wizards.com/dnd/Tools.aspx"))
 links = page.css("a[class=ContentBoxLink]")
 links.each do |link|
+#wraps the link in a div, for easier editing
 	x.write("<div>" + link + "</div>\n")
 	end
 end
