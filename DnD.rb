ebeb5827da898300cd1502c297889195de622b80
davemerritt <davidmerritt6@gmail.com>
2013-11-03 17:52:29 -0500

    Now it writes the file

--- DnD.rb
+++ DnD.rb
@@ -2,8 +2,14 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+File.open('DnD.txt', 'w') do |x|
 page = Nokogiri::HTML(open("http://www.wizards.com/dnd/Tools.aspx"))
 links = page.css("a[class=ContentBoxLink]")
-links.each { |link| puts link['href']}
+links.each do |link|
+	x.write("<div>" + link + "</div>\n")
+	end
+end
+
 #Hey, it does stuff! 
 #Neat!
+#Changes, now it makes the file, but not a useful file that I, or anyone, wants.
